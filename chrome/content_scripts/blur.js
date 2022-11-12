62f14c4f3ae7ebcc034e4bff14604e7a26686f03
Hannah Kolbeck <hskolbeck@gmail.com>
2022-11-11 16:10:05 -0800

    Patch up centering of 😒

--- chrome/content_scripts/blur.js
+++ chrome/content_scripts/blur.js
@@ -6,14 +6,15 @@ blurDiv.style.width = "100vw"
 blurDiv.style.height = "100vh"
 blurDiv.style.zIndex = "2147483647"
 blurDiv.style.backdropFilter = "blur(20px)"
+blurDiv.style.display = "flex"
+blurDiv.style.alignItems = "center"
+blurDiv.style.justifyContent = "center"
 
 blurDiv.onclick = () => blurDiv.style.display = "none"
 
 const emojiDiv = document.createElement("div");
 emojiDiv.style.fontSize = "10rem"
 emojiDiv.style.textAlign = "center"
-emojiDiv.style.gridArea = "c"
-emojiDiv.style.marginTop = "45vh"
 emojiDiv.innerText = "😒"
 
 blurDiv.appendChild(emojiDiv)
